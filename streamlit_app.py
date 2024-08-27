796191106f05f993d0f3c94d79d77a177c9c6c56
Maxim <maxim.oweyssi@est.org.uk>
2024-08-27 15:02:45 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -382,7 +382,7 @@ if Scenario == "2. Remove VAT from electricity bills only and don't touch levy c
     selected_scenario = scenario2
     st.sidebar.text("Yearly savings just from scenario are £" + str(round(scenario2saving(HomeUse_ele,HomeUse_gas,type=Before_Heating,perc_offpeak=OffPeak_percentage))))
 
-if Scenario == "3. Introduce a clean heat discount":
+if Scenario == "3. Introduce a flat clean heat discount":
     selected_scenario = scenario3
     st.sidebar.text("Yearly savings just from scenario are £" + str(round(scenario3saving(HomeUse_ele,HomeUse_gas,type=Before_Heating,perc_offpeak=OffPeak_percentage,electricity_discount_kWh=CleanHeatDiscount))))
     
